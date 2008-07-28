d3f9215c2ece76eb63891b1faca092d38a99ec3e
Justin Palmer <justin@activereload.net>
2008-07-28 16:21:51 -0700

    Add SenTestCase subclass template and setup and teardown snippets

---
+++ Templates/SenTestCase Subclass.tmTemplate/class_in.m
@@ -0,0 +1,16 @@
+//
+//  ${TM_NEW_FILE_BASENAME}
+
+
+#import "GTMSenTestCase.h"
+#import "${TESTING_FILE}.h"
+
+@interface ${TM_NEW_FILE_BASENAME} : SenTestCase {}
+@end
+
+@implementation ${TM_NEW_FILE_BASENAME}
+- (void) setUp
+{
+
+}
+@end
